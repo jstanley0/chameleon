0514a3ba807a3d8c86337f5cd1c378199a2a699c
Jeremy Stanley <jeremy@instructure.com>
2015-01-24 11:11:51 -0700

    various fixes
    switch to inverting PWM mode, apply a fudge factor to the
    color balance, and increase saturation a bit more
    
    it can almost see blue now

--- main.c
+++ main.c
@@ -5,6 +5,10 @@
 // Source code (c) 2015 by Jeremy Stanley
 // Licensed under GNU GPL v2 or later
 
+// if defined, blink out the high 4 bits of RGB values instead of mixing colors
+// (bright blink = 1, low blink = 0)
+//#define DIAGNOSTIC
+
 // inputs:
 // pin 13. PA0 (ADC0) = photoresistor
 
@@ -38,11 +42,51 @@ void init_adc()
 	// select AVCC reference, ADC1 source
 	ADMUX = 0;
 	
+	// left-align results
+	ADCSRB = (1 << ADLAR);
+	
 	// enable ADC and start conversions at 1/128 prescaler
 	ADCSRA = (1 << ADEN) | (1 << ADSC) | (1 << ADIE) | (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);
 }
 
 volatile uint8_t illum = 0;
+
+#ifdef DIAGNOSTIC
+
+void display_color(uint8_t color, uint8_t intensity)
+{
+	R_INTENSITY = 255 - ((color == 0) ? intensity : 0);
+	G_INTENSITY = 255 - ((color == 1) ? intensity : 0);
+	B_INTENSITY = 255 - ((color == 2) ? intensity : 0);
+}
+
+#define BIT 300
+#define INTERBIT 150
+#define INTERBYTE 500
+
+void display_bit(uint8_t bit)
+{
+	display_color(illum, bit ? 255 : 7);
+	_delay_ms(BIT);
+	display_color(4, 0);
+	_delay_ms(INTERBIT);
+}
+
+void display_byte(uint8_t x)
+{
+	uint8_t bit = 0x80;
+	
+	cli();
+	while(bit > 0x8)
+	{
+		display_bit(x & bit);
+		bit >>= 1;
+	}
+	_delay_ms(INTERBYTE);
+	sei();
+}
+
+#endif	// DIAGNOSTIC
 
 void init_illum()
 {
@@ -59,22 +103,25 @@ void cycle_illum()
 
 void init_pwm()
 {
-	R_INTENSITY = 0;
-	G_INTENSITY = 0;
-	B_INTENSITY = 0;
+	// we use 0 = max brightness; 255 = off
+	// because otherwise we cannot get a 0% duty cycle for a color
+	// (and 255/256 is indistinguishable from full-on)
+	R_INTENSITY = 255;
+	G_INTENSITY = 255;
+	B_INTENSITY = 255;
 
 	// timer0
-	// select non-inverting fast PWM mode for red (0A) and green (0B)
-	TCCR0A = (1 << COM0A1) | (1 << COM0B1) | (1 << WGM01) | (1 << WGM00);
+	// select inverting fast PWM mode for red (0A) and green (0B)
+	TCCR0A = (1 << COM0A1) | (1 << COM0A0) | (1 << COM0B1) | (1 << COM0B0) | (1 << WGM01) | (1 << WGM00);
 	TCCR0B = (1 << CS01);	// 1/8 prescaler
 	
 	// timer1
-	// select non-inverting 8-bit fast PWM mode for blue (1A)
-	TCCR1A = (1 << COM1A1) | (1 << WGM10);
+	// select inverting 8-bit fast PWM mode for blue (1A)
+	TCCR1A = (1 << COM1A1) | (1 << COM1A0) | (1 << WGM10);
 	TCCR1B = (1 << WGM12) | (1 << CS11); // 1/8 prescaler
 }
 
-uint8_t clamp(int16_t val)
+uint8_t clamp(int val)
 {
 	if (val < 0)
 		return 0;
@@ -85,34 +132,40 @@ uint8_t clamp(int16_t val)
 }
 
 // result is the brightness result for the color given by illum
-// (0 == full brightness; 1023 = total darkness)
-void update_pwm(uint16_t result)
+// (0 == full brightness; 255 = total darkness)
+void update_pwm(uint8_t result)
 {
 	static uint8_t rgb[3] = {0, 0, 0};
+	static const int fudge[3] = {13, 12, 11};
 	uint8_t sat[3];
-	uint8_t i, tc = (1023 - result) >> 2;
+	uint8_t i, tc = result;
+#ifdef DIAGNOSTIC
+	display_byte(tc); return;
+#else
 
-	rgb[illum] = tc;
-	
-	// a quick-and-dirty way to amp up the saturation
+	// fix color balance
+	rgb[illum] = clamp((tc * fudge[illum]) / 11);
+
+	// amp up the saturation
 	uint8_t avg = (rgb[0] + rgb[1] + rgb[2]) / 3;
 	for(i = 0; i < 3; ++i) {
-		int16_t d = 6 * (rgb[i] - avg);
+		int16_t d = 8 * (rgb[i] - avg);
 		sat[i] = clamp(d + rgb[i]);
 	}
 
 	R_INTENSITY = sat[0];
 	G_INTENSITY = sat[1];
 	B_INTENSITY = sat[2];
+#endif
 }
 
 ISR(ADC_vect)
 {
 	// average a whole bunch of readings
-	static uint32_t accum = 0;
-	static uint8_t  count = 0;
+	static int16_t accum = 0;
+	static uint8_t count = 0;
 	
-	accum += ADC;
+	accum += ADCH;
 	if (++count == 128) {
 		uint16_t result = accum >> 7;
 		count = 0;
